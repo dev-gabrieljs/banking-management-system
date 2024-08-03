862fadef2241c117f372e49ccd10cad37d08c734
Gajsilva <devgabrielsj@gmail.com>
2024-08-03 14:17:59 -0300

    Initial commit

---
+++ src/main/java/com/br/account/entity/Feature.java
@@ -0,0 +1,13 @@
+package com.br.account.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_feature")
+public class Feature extends BaseItem {
+
+
+
+}
---
+++ src/main/java/com/br/account/entity/News.java
@@ -0,0 +1,10 @@
+package com.br.account.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_news")
+public class News extends BaseItem{
+
+}
